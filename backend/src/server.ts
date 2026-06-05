773761761f0c86549bac64666a81a02771df2001
Veronica <veronica.marchiori@estudante.uffs.edu.br>
2026-06-04 23:50:24 -0300

    Tarefa: Inicialização do backend
    ---Criação das dependências do projeto backend
    ---Primeiros testes com um listen para ver se a conexão funciona
    ---Instalação das dependências necessárias do node, tsconfig e etc
    ---Compartimentalização, como de costume em meus projetos

---
+++ backend/src/server.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req,res) => {
+    res.send("API funcionando??");
+
+});
+//apenas pra eu conseguir testar se está funcionando
+const PORT = process.env.PORT || 3000;
+app.listen(3000, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+
+});
+
