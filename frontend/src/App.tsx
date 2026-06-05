3ccfd87bed8112a5a0dde2f358c2a7c9c40362ca
Veronica <veronica.marchiori@estudante.uffs.edu.br>
2026-06-05 07:02:06 -0300

    Tarefa: Corrigindo casing dos imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Navbar from './components/Navbar'
+import Navbar from './components/navbar'
 import { Routes, Route} from 'react-router-dom'
 
 import ListBarbershops from './pages/ListBarbershops'
--- frontend/src/pages/RegisterBarberShop.tsx
+++ frontend/src/pages/RegisterBarberShop.tsx
@@ -1,4 +1,4 @@
-import Form from "../components/Form";
+import Form from "../components/form";
 import Card from "../components/card";
 import { Scissors } from "lucide-react";
 
