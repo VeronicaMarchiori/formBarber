9a0ca42eb4ed682aff017d59b8ec663a1fe099d9
Veronica <veronica.marchiori@estudante.uffs.edu.br>
2026-06-05 01:55:45 -0300

    Tarefa: Criação do update e do delete para completar o crud inicial
    ---Alterei controllers e routes para as operações de put e delete para eventualmente adicionar edição e deletar nos cards
    ---Adicionadas as novas rotas no router
    ---Partindo para a integração das funcionalidades atuais no frontend e correções. Etapas finais.

--- backend/src/controllers/barbershopController.ts
+++ backend/src/controllers/barbershopController.ts
@@ -43,6 +43,94 @@ export async function createBarbershop(req: Request, res: Response) {
     }
 }
 
+export async function updateBarbershop(req: Request, res: Response){
+    try {
+        const { id } = req.params;
+
+        const{
+            name,
+            owner,
+            email,
+            phone,
+            city,
+            state,
+            chairs,
+        } = req.body;
+
+        const query = `
+        UPDATE barbershops
+        SET 
+        name= $1,
+        owner= $2,
+        email= $3,
+        phone= $4,
+        city= $5,
+        state = $6,
+        chairs = $7
+        WHERE id= $8
+        RETURNING *;
+        `;
+
+        const values = [
+            name,
+            owner,
+            email,
+            phone,  
+            city,   
+            state,
+            chairs,
+            id,
+        ];
+
+        const result = await pool.query(query, values);
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                message: "Barbearia não encontrada",
+            });
+        }
+
+        return res.status(200).json({
+            message: "Barbearia atualizada com sucesso",
+            barbershop: result.rows[0],
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        return res.status(500).json({
+            messagee: "Erro ao atualizar barbearia",
+        });
+    }
+}
+
+export async function deleteBarbershop(req: Request, res: Response){
+    try{
+        const {id} = req.params;
+
+        const result=await pool.query(
+            "DELETE FROM barbershops WHERE id = $1 RETURNING *;",
+            [id]
+        );
+
+        if (result.rows.length ===0) {
+            return res.status(404).json({
+                message: "Barbearia não encontrada",
+            });
+        }
+
+        return res.status(200).json({
+            message: "Barbearia removida com sucesso",
+        });
+    } catch (error){
+        console.error(error);
+
+        return res.status(500).json({
+            message: "Erro ao remover barbearia",
+        });
+    }
+}
+
 export async function getBarbershops(req: Request, res: Response){
     try {
         const result = await pool.query(
--- backend/src/routes/barbershopRoutes.ts
+++ backend/src/routes/barbershopRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { createBarbershop, getBarbershops } from "../controllers/barbershopController";
+import { createBarbershop, getBarbershops, updateBarbershop, deleteBarbershop } from "../controllers/barbershopController";
 
 const router = Router();
 
 router.post("/", createBarbershop);
 router.get("/", getBarbershops);
+router.put("/:id", updateBarbershop);
+router.delete("/:id", deleteBarbershop);
 
 export default router;
